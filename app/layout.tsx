e4b96a0b255f374b28764beb0f6913910675b92d
bgyordan <yordan.yordanov@csop-varna.bg>
2026-09-19 19:56:57 +0300

    Add CookieBanner component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import ScrollTop from "./components/ScrollTop";
 import AccessibilityWidget from "./components/AccessibilityWidget";
+import CookieBanner from "./components/CookieBanner";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://web.csop-varna.bg"),
@@ -70,7 +71,7 @@ export default function RootLayout({
           }}
         />
       </head>
-            <body>{children}<ScrollTop /><AccessibilityWidget /></body>
+                <body>{children}<ScrollTop /><AccessibilityWidget /><CookieBanner /></body>
     </html>
   );
 }

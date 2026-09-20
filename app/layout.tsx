42f87aa28bb4f9511d4d04cf082f5d3a801d7680
bgyordan <yordan.yordanov@csop-varna.bg>
2026-09-20 20:35:15 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,10 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
           href="/noop.css"
           rel="stylesheet"
         />
